d47eda0cebffe21ff00612b06ecf3020e495127c
jbg77 <jbgaume@gmail.com>
2016-08-03 20:50:05 +0200

    Update to 2.1.1
    Possible to update to the last version ?

--- scripts/linux/install-phantomjs.sh
+++ scripts/linux/install-phantomjs.sh
@@ -10,7 +10,7 @@ set -e
 # Install PhantomJS
 sudo apt-get -y install libfreetype6 libfreetype6-dev fontconfig > /dev/null
 ARCH=`uname -m`
-PHANTOMJS_VERSION=1.9.8
+PHANTOMJS_VERSION=2.1.1
 
 cd /usr/local/share/
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2 > /dev/null
